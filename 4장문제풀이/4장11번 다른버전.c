886f2c4d064480892fd45597bf0db3bcc11eaa8a
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-04-21 13:41:47 +0900

    Update 4장11번 다른버전.c

--- 4장문제풀이/4장11번 다른버전.c
+++ 4장문제풀이/4장11번 다른버전.c
@@ -2,64 +2,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_STACK_SIZE 100
-
 typedef int element;
-int top = -1;
+typedef struct {
+	element data[MAX_STACK_SIZE];
+	int top;
+}StackType;
 
-element stack[MAX_STACK_SIZE];
-
-int is_empty() {
-	return (top == -1);
+void init_stack(StackType* s) {
+	s->top = -1;
 }
-int is_full() {
-	return (top == MAX_STACK_SIZE - 1);
+int is_full(StackType* s) {
+	return s->top == MAX_STACK_SIZE - 1;
 }
-
-void push(element item) {
-	if (is_full()) {
+int is_empty(StackType* s) {
+	return s->top == -1;
+}
+void push(StackType* s, element item) {
+	if (is_full(s)) {
 		fprintf(stderr, "스택 포화 에러\n");
 		return;
 	}
-	else
-		stack[++top] = item;
+	s->data[++(s->top)] = item;
 }
-
-int pop() {
-	if (is_empty()) {
+element pop(StackType* s) {
+	if (is_empty(s)) {
 		fprintf(stderr, "스택 공백 에러\n");
-		exit(1);
+		return;
 	}
-	else
-		return stack[top--];
+	return s->data[(s->top)--];
 }
-
-int peek() {
-	if (is_empty()) {
+element peek(StackType* s) {
+	if (is_empty(s)) {
 		fprintf(stderr, "스택 공백 에러\n");
-		exit(1);
+		return;
 	}
-	else
-		return stack[top];
+	return s->data[(s->top)];
 }
-
 int main() {
-	element item;
-	char str[MAX_STACK_SIZE];
+	StackType s;
+
+	init_stack(&s);
+	char test[MAX_STACK_SIZE];
 	int count = 1;
 	printf("수식 : ");
-	scanf("%s", str);
-
+	scanf("%s", test);
 	printf("괄호 수 : ");
 	for (int i = 0; i < MAX_STACK_SIZE; i++) {
-		if (str[i] == '(') {
-			push(count);
+		if (test[i] == NULL)break;
+		if (test[i] == '(') {
 			printf("%d ", count);
+			push(&s, count);
 			count++;
 		}
-		else if (str[i] == ')') {
-			printf("%d ", pop());
+		else { //test[i] == ')'
+			printf("%d ", pop(&s));
 		}
-		if (str[i] == NULL)
-			break;
 	}
 }
